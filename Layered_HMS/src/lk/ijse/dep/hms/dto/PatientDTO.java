676b533303cac817a1b2e5d2a95f9faa3229efe2
NipunChathuranga <nipunc394@gmail.com>
2019-10-09 13:02:20 +0530

    DTOs Added.

--- Layered_HMS/src/lk/ijse/dep/hms/dto/PatientDTO.java
+++ Layered_HMS/src/lk/ijse/dep/hms/dto/PatientDTO.java
@@ -1,4 +1,94 @@
 package lk.ijse.dep.hms.dto;
 
 public class PatientDTO {
+
+    private String patientid;
+    private String firstname;
+    private String lastname;
+    private String gender;
+    private String city;
+    private String email;
+    private String password;
+
+    public PatientDTO() {
+    }
+
+    public PatientDTO(String patientid, String firstname, String lastname, String gender, String city, String email, String password) {
+        this.patientid = patientid;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.city = city;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPatientid() {
+        return patientid;
+    }
+
+    public void setPatientid(String patientid) {
+        this.patientid = patientid;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PatientDTO{" +
+                "patientid='" + patientid + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", gender='" + gender + '\'' +
+                ", city='" + city + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- Layered_HMS/src/lk/ijse/dep/hms/dto/PrescriptionDTO.java
+++ Layered_HMS/src/lk/ijse/dep/hms/dto/PrescriptionDTO.java
@@ -1,4 +1,53 @@
 package lk.ijse.dep.hms.dto;
 
+import java.sql.Date;
+import java.util.List;
+
 public class PrescriptionDTO {
+
+    private String prescriptionid;
+    private String appoinmentid;
+    private List<PrescriptionDetailDTO> prescriptionDetails;
+
+    public PrescriptionDTO() {
+    }
+
+    public PrescriptionDTO(String prescriptionid, String appoinmentid, List<PrescriptionDetailDTO> prescriptionDetails) {
+        this.prescriptionid = prescriptionid;
+        this.appoinmentid = appoinmentid;
+        this.prescriptionDetails = prescriptionDetails;
+    }
+
+    public String getPrescriptionid() {
+        return prescriptionid;
+    }
+
+    public void setPrescriptionid(String prescriptionid) {
+        this.prescriptionid = prescriptionid;
+    }
+
+    public String getAppoinmentid() {
+        return appoinmentid;
+    }
+
+    public void setAppoinmentid(String appoinmentid) {
+        this.appoinmentid = appoinmentid;
+    }
+
+    public List<PrescriptionDetailDTO> getPrescriptionDetails() {
+        return prescriptionDetails;
+    }
+
+    public void setPrescriptionDetails(List<PrescriptionDetailDTO> prescriptionDetails) {
+        this.prescriptionDetails = prescriptionDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "PrescriptionDTO{" +
+                "prescriptionid='" + prescriptionid + '\'' +
+                ", appoinmentid='" + appoinmentid + '\'' +
+                ", prescriptionDetails=" + prescriptionDetails +
+                '}';
+    }
 }
